d2d3c1c6dbbd4078c142303abce17f81772ee5ef
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-03-09 01:24:59 +0000

    using i2c_slave.h

--- SparrowSV3-Right/firmware.c
+++ SparrowSV3-Right/firmware.c
@@ -1,5 +1,6 @@
 #include "ch32fun.h"
 #include "ch32v003_GPIO_branchless.h"
+#include "i2c_slave.h"
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -34,59 +35,59 @@ typedef struct
 	uint8_t length;
 } I2CMosiEvent;
 
-void I2C1_EV_IRQHandler(void) __attribute__((interrupt));
-void I2C1_ER_IRQHandler(void) __attribute__((interrupt));
+// void I2C1_EV_IRQHandler(void) __attribute__((interrupt));
+// void I2C1_ER_IRQHandler(void) __attribute__((interrupt));
 
-void init_i2c_slave(uint8_t address)
-{
-	// https://github.com/cnlohr/ch32v003fun/blob/master/examples/i2c_slave/i2c_slave.h
-	RCC->CFGR0 &= ~(0x1F << 11);
+// void init_i2c_slave(uint8_t address)
+// {
+// 	// https://github.com/cnlohr/ch32v003fun/blob/master/examples/i2c_slave/i2c_slave.h
+// 	RCC->CFGR0 &= ~(0x1F << 11);
 
-	RCC->APB2PCENR |= RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD | RCC_APB2Periph_AFIO;
-	RCC->APB1PCENR |= RCC_APB1Periph_I2C1;
+// 	RCC->APB2PCENR |= RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD | RCC_APB2Periph_AFIO;
+// 	RCC->APB1PCENR |= RCC_APB1Periph_I2C1;
 
-	// PC1 is SDA, 10MHz Output, alt func, open-drain
-	GPIOC->CFGLR &= ~(0xf << (4 * 1));
-	GPIOC->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_OD_AF) << (4 * 1);
+// 	// PC1 is SDA, 10MHz Output, alt func, open-drain
+// 	GPIOC->CFGLR &= ~(0xf << (4 * 1));
+// 	GPIOC->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_OD_AF) << (4 * 1);
 
-	// PC2 is SCL, 10MHz Output, alt func, open-drain
-	GPIOC->CFGLR &= ~(0xf << (4 * 2));
-	GPIOC->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_OD_AF) << (4 * 2);
+// 	// PC2 is SCL, 10MHz Output, alt func, open-drain
+// 	GPIOC->CFGLR &= ~(0xf << (4 * 2));
+// 	GPIOC->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_OD_AF) << (4 * 2);
 
-	// Reset I2C1 to init all regs
-	RCC->APB1PRSTR |= RCC_APB1Periph_I2C1;
-	RCC->APB1PRSTR &= ~RCC_APB1Periph_I2C1;
+// 	// Reset I2C1 to init all regs
+// 	RCC->APB1PRSTR |= RCC_APB1Periph_I2C1;
+// 	RCC->APB1PRSTR &= ~RCC_APB1Periph_I2C1;
 
-	I2C1->CTLR1 |= I2C_CTLR1_SWRST;
-	I2C1->CTLR1 &= ~I2C_CTLR1_SWRST;
+// 	I2C1->CTLR1 |= I2C_CTLR1_SWRST;
+// 	I2C1->CTLR1 &= ~I2C_CTLR1_SWRST;
 
-	// Set module clock frequency
-	uint32_t prerate = 2000000; // I2C Logic clock rate, must be higher than the bus clock rate
-	I2C1->CTLR2 |= (FUNCONF_SYSTEM_CORE_CLOCK / prerate) & I2C_CTLR2_FREQ;
+// 	// Set module clock frequency
+// 	uint32_t prerate = 2000000; // I2C Logic clock rate, must be higher than the bus clock rate
+// 	I2C1->CTLR2 |= (FUNCONF_SYSTEM_CORE_CLOCK / prerate) & I2C_CTLR2_FREQ;
 
-	// Enable interrupts
-	I2C1->CTLR2 |= I2C_CTLR2_ITBUFEN;
-	I2C1->CTLR2 |= I2C_CTLR2_ITEVTEN; // Event interrupt
-	I2C1->CTLR2 |= I2C_CTLR2_ITERREN; // Error interrupt
+// 	// Enable interrupts
+// 	I2C1->CTLR2 |= I2C_CTLR2_ITBUFEN;
+// 	I2C1->CTLR2 |= I2C_CTLR2_ITEVTEN; // Event interrupt
+// 	I2C1->CTLR2 |= I2C_CTLR2_ITERREN; // Error interrupt
 
-	NVIC_EnableIRQ(I2C1_EV_IRQn); // Event interrupt
-	NVIC_SetPriority(I2C1_EV_IRQn, 2 << 4);
-	NVIC_EnableIRQ(I2C1_ER_IRQn); // Error interrupt
-	// Set clock configuration
-	uint32_t clockrate = 1000000;																	 // I2C Bus clock rate, must be lower than the logic clock rate
-	I2C1->CKCFGR = ((FUNCONF_SYSTEM_CORE_CLOCK / (3 * clockrate)) & I2C_CKCFGR_CCR) | I2C_CKCFGR_FS; // Fast mode 33% duty cycle
-	// I2C1->CKCFGR = ((APB_CLOCK/(25*clockrate))&I2C_CKCFGR_CCR) | I2C_CKCFGR_DUTY | I2C_CKCFGR_FS; // Fast mode 36% duty cycle
-	// I2C1->CKCFGR = (APB_CLOCK/(2*clockrate))&I2C_CKCFGR_CCR; // Standard mode good to 100kHz
+// 	NVIC_EnableIRQ(I2C1_EV_IRQn); // Event interrupt
+// 	NVIC_SetPriority(I2C1_EV_IRQn, 2 << 4);
+// 	NVIC_EnableIRQ(I2C1_ER_IRQn); // Error interrupt
+// 	// Set clock configuration
+// 	uint32_t clockrate = 1000000;																	 // I2C Bus clock rate, must be lower than the logic clock rate
+// 	I2C1->CKCFGR = ((FUNCONF_SYSTEM_CORE_CLOCK / (3 * clockrate)) & I2C_CKCFGR_CCR) | I2C_CKCFGR_FS; // Fast mode 33% duty cycle
+// 	// I2C1->CKCFGR = ((APB_CLOCK/(25*clockrate))&I2C_CKCFGR_CCR) | I2C_CKCFGR_DUTY | I2C_CKCFGR_FS; // Fast mode 36% duty cycle
+// 	// I2C1->CKCFGR = (APB_CLOCK/(2*clockrate))&I2C_CKCFGR_CCR; // Standard mode good to 100kHz
 
-	// Set I2C address
-	I2C1->OADDR1 = address << 1;
+// 	// Set I2C address
+// 	I2C1->OADDR1 = address << 1;
 
-	// Enable I2C
-	I2C1->CTLR1 |= I2C_CTLR1_PE;
+// 	// Enable I2C
+// 	I2C1->CTLR1 |= I2C_CTLR1_PE;
 
-	// Acknowledge the first address match event when it happens
-	I2C1->CTLR1 |= I2C_CTLR1_ACK;
-}
+// 	// Acknowledge the first address match event when it happens
+// 	I2C1->CTLR1 |= I2C_CTLR1_ACK;
+// }
 
 uint16_t i2c_scan_start_position = 0;
 uint16_t i2c_scan_length = 0;
@@ -96,114 +97,114 @@ bool on_mosi_event = false;
 I2CMosiEvent latest_mosi_event = {0, 0};
 bool address2matched = false;
 
-void I2C1_EV_IRQHandler(void)
-{
-	uint16_t STAR1, STAR2 __attribute__((unused));
-	STAR1 = I2C1->STAR1;
-	STAR2 = I2C1->STAR2;
+// void I2C1_EV_IRQHandler(void)
+// {
+// 	uint16_t STAR1, STAR2 __attribute__((unused));
+// 	STAR1 = I2C1->STAR1;
+// 	STAR2 = I2C1->STAR2;
 
-	// printf("EV STAR1: 0x%04x STAR2: 0x%04x\r\n", STAR1, STAR2);
+// 	// printf("EV STAR1: 0x%04x STAR2: 0x%04x\r\n", STAR1, STAR2);
 
-	I2C1->CTLR1 |= I2C_CTLR1_ACK;
+// 	I2C1->CTLR1 |= I2C_CTLR1_ACK;
 
-	if (STAR1 & I2C_STAR1_ADDR) // 0x0002
-	{
-		// 最初のイベント
-		// read でも write でも必ず最初に呼ばれる
-		address2matched = !!(STAR2 & I2C_STAR2_DUALF);
-		// printf("ADDR %d\r\n", address2matched);
-		i2c_firse_byte = true;
-		i2c_mosi_event = false;
-		i2c_scan_start_position = 0;
-		i2c_scan_length = 0;
-	}
+// 	if (STAR1 & I2C_STAR1_ADDR) // 0x0002
+// 	{
+// 		// 最初のイベント
+// 		// read でも write でも必ず最初に呼ばれる
+// 		address2matched = !!(STAR2 & I2C_STAR2_DUALF);
+// 		// printf("ADDR %d\r\n", address2matched);
+// 		i2c_firse_byte = true;
+// 		i2c_mosi_event = false;
+// 		i2c_scan_start_position = 0;
+// 		i2c_scan_length = 0;
+// 	}
 
-	if (STAR1 & I2C_STAR1_RXNE) // 0x0040
-	{
-		if (i2c_firse_byte)
-		{
-			i2c_scan_start_position = I2C1->DATAR;
-			i2c_scan_length = 0;
-			i2c_firse_byte = false;
-			i2c_mosi_event = true;
-			// printf("RXNE write event: first_write 0x%x\r\n", i2c_scan_start_position);
-		}
-		else
-		{
-			uint16_t pos = i2c_scan_start_position + i2c_scan_length;
-			uint8_t v = I2C1->DATAR;
-			// printf("RXNE write event: pos:0x%x v:0x%x\r\n", pos, v);
-			if (pos < 32)
-			{
-				i2c_registers[pos] = v;
-				i2c_scan_length++;
-			}
-		}
-	}
+// 	if (STAR1 & I2C_STAR1_RXNE) // 0x0040
+// 	{
+// 		if (i2c_firse_byte)
+// 		{
+// 			i2c_scan_start_position = I2C1->DATAR;
+// 			i2c_scan_length = 0;
+// 			i2c_firse_byte = false;
+// 			i2c_mosi_event = true;
+// 			// printf("RXNE write event: first_write 0x%x\r\n", i2c_scan_start_position);
+// 		}
+// 		else
+// 		{
+// 			uint16_t pos = i2c_scan_start_position + i2c_scan_length;
+// 			uint8_t v = I2C1->DATAR;
+// 			// printf("RXNE write event: pos:0x%x v:0x%x\r\n", pos, v);
+// 			if (pos < 32)
+// 			{
+// 				i2c_registers[pos] = v;
+// 				i2c_scan_length++;
+// 			}
+// 		}
+// 	}
 
-	if (STAR1 & I2C_STAR1_TXE) // 0x0080
-	{
-		if (i2c_firse_byte)
-		{
-			i2c_scan_start_position = I2C1->DATAR;
-			i2c_scan_length = 0;
-			i2c_firse_byte = false;
-			i2c_mosi_event = true;
-			// printf("TXE write event: first_write:%x\r\n", i2c_scan_start_position);
-		}
-		else
-		{
-			// 1byte の read イベント（slave -> master）
-			// 1byte 送信
-			uint16_t pos = i2c_scan_start_position + i2c_scan_length;
-			uint8_t v = 0x00;
-			if (pos < 32)
-			{
-				v = i2c_registers[pos];
-			}
-			// printf("TXE write event: pos:0x%x v:0x%x\r\n", pos, v);
-			I2C1->DATAR = v;
-			i2c_scan_length++;
+// 	if (STAR1 & I2C_STAR1_TXE) // 0x0080
+// 	{
+// 		if (i2c_firse_byte)
+// 		{
+// 			i2c_scan_start_position = I2C1->DATAR;
+// 			i2c_scan_length = 0;
+// 			i2c_firse_byte = false;
+// 			i2c_mosi_event = true;
+// 			// printf("TXE write event: first_write:%x\r\n", i2c_scan_start_position);
+// 		}
+// 		else
+// 		{
+// 			// 1byte の read イベント（slave -> master）
+// 			// 1byte 送信
+// 			uint16_t pos = i2c_scan_start_position + i2c_scan_length;
+// 			uint8_t v = 0x00;
+// 			if (pos < 32)
+// 			{
+// 				v = i2c_registers[pos];
+// 			}
+// 			// printf("TXE write event: pos:0x%x v:0x%x\r\n", pos, v);
+// 			I2C1->DATAR = v;
+// 			i2c_scan_length++;
 
-			// 送信の度にWatch Dog Timerをリセット
-			IWDG->CTLR = CTLR_KEY_Reload;
-		}
-	}
+// 			// 送信の度にWatch Dog Timerをリセット
+// 			IWDG->CTLR = CTLR_KEY_Reload;
+// 		}
+// 	}
 
-	if (STAR1 & I2C_STAR1_STOPF)
-	{
-		I2C1->CTLR1 &= ~(I2C_CTLR1_STOP);
-		if (i2c_mosi_event)
-		{
-			// printf("on_mosi_event\r\n");
-			latest_mosi_event.reg = i2c_scan_start_position;
-			latest_mosi_event.length = i2c_scan_length;
-			on_mosi_event = true;
-		}
-	}
-}
+// 	if (STAR1 & I2C_STAR1_STOPF)
+// 	{
+// 		I2C1->CTLR1 &= ~(I2C_CTLR1_STOP);
+// 		if (i2c_mosi_event)
+// 		{
+// 			// printf("on_mosi_event\r\n");
+// 			latest_mosi_event.reg = i2c_scan_start_position;
+// 			latest_mosi_event.length = i2c_scan_length;
+// 			on_mosi_event = true;
+// 		}
+// 	}
+// }
 
-void I2C1_ER_IRQHandler(void)
-{
-	uint16_t STAR1 = I2C1->STAR1;
+// void I2C1_ER_IRQHandler(void)
+// {
+// 	uint16_t STAR1 = I2C1->STAR1;
 
-	// printf("ER STAR1: 0x%04x\r\n", STAR1);
+// 	// printf("ER STAR1: 0x%04x\r\n", STAR1);
 
-	if (STAR1 & I2C_STAR1_BERR)			  // 0x0100
-	{									  // Bus error
-		I2C1->STAR1 &= ~(I2C_STAR1_BERR); // Clear error
-	}
+// 	if (STAR1 & I2C_STAR1_BERR)			  // 0x0100
+// 	{									  // Bus error
+// 		I2C1->STAR1 &= ~(I2C_STAR1_BERR); // Clear error
+// 	}
 
-	if (STAR1 & I2C_STAR1_ARLO)			  // 0x0200
-	{									  // Arbitration lost error
-		I2C1->STAR1 &= ~(I2C_STAR1_ARLO); // Clear error
-	}
+// 	if (STAR1 & I2C_STAR1_ARLO)			  // 0x0200
+// 	{									  // Arbitration lost error
+// 		I2C1->STAR1 &= ~(I2C_STAR1_ARLO); // Clear error
+// 	}
 
-	if (STAR1 & I2C_STAR1_AF)			// 0x0400
-	{									// Acknowledge failure
-		I2C1->STAR1 &= ~(I2C_STAR1_AF); // Clear error
-	}
-}
+// 	if (STAR1 & I2C_STAR1_AF)			// 0x0400
+// 	{									// Acknowledge failure
+// 		I2C1->STAR1 &= ~(I2C_STAR1_AF); // Clear error
+// 	}
+// }
 
 void print_bits(uint8_t value)
 {
@@ -211,6 +212,16 @@ void print_bits(uint8_t value)
 	{
 		printf("%d", (value >> i) & 1);
 	}
+}
+
+void onWrite(uint8_t reg, uint8_t length)
+{
+	IWDG->CTLR = CTLR_KEY_Reload;
+}
+
+void onRead(uint8_t reg)
+{
+	IWDG->CTLR = CTLR_KEY_Reload;
 }
 
 void setup()
@@ -229,12 +240,15 @@ void setup()
 		GPIO_pinMode(ROW_PINS[i], GPIO_pinMode_I_pullDown, GPIO_Speed_10MHz);
 	}
 
-	init_i2c_slave(I2C_ADDRESS);
-
 	for (uint8_t i = 0; i < 32; i++)
 	{
 		i2c_registers[i] = 0x00;
 	}
+
+	// init_i2c_slave(I2C_ADDRESS);
+	funPinMode(PC1, GPIO_CFGLR_OUT_10Mhz_AF_OD); // SDA
+	funPinMode(PC2, GPIO_CFGLR_OUT_10Mhz_AF_OD); // SCL
+	SetupI2CSlave(I2C_ADDRESS, i2c_registers, sizeof(i2c_registers), onWrite, onRead, false);
 
 	// Independent Watch Dog Timerを2sのカウンタでセット
 	IWDG->CTLR = IWDG_WriteAccess_Enable;
